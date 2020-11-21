f25408d9eca5f62508f1fef38188cb21db40df3c
Esteban Jaramillo Sarmiento <26073885+Steve0929@users.noreply.github.com>
2020-11-21 11:16:15 -0500

    SOAP server

--- server.js
+++ server.js
@@ -1,34 +1,55 @@
 var soap = require('soap');
 var http = require('http');
-
+var axios = require('axios');
+const URL = "http://54.198.239.79:5230/graphql"; //proxy url
 
 var service = {
     ws: {
         questionList: {
             questions : async function(args) {
                 var category = args.category
-                return { res : "you asked for questions from: "+category };
+                const questions = await axios.post(URL, {
+                query: `
+                  query {
+                    gameQuestions(category: "${category}") {
+                      categoria
+                      ERROR
+                      preguntas{
+                        statement
+                        optionA
+                        optionB
+                        optionC
+                        optionD
+                        image
+                        ans
+                      }
+                     }
+                    }
+                    `
+                  })
+                if(questions &&  questions.data &&  questions.data.data &&  questions.data.data.gameQuestions.preguntas){
+                  const rngQuestion = questions.data.data.gameQuestions.preguntas[Math.floor(Math.random() * questions.data.data.gameQuestions.preguntas.length)];;
+                  return {  res : "Random question from: "+category,
+                            statement: rngQuestion.statement,
+                            optionA:  rngQuestion.optionA,
+                            optionB: rngQuestion.optionB,
+                            optionC: rngQuestion.optionC,
+                            optionD: rngQuestion.optionD,
+                            image: rngQuestion.image,
+                            ans:  rngQuestion.ans
+                         };
+                }
+                else{
+                  if(questions.data.data.gameQuestions.ERROR){
+                    return {res: questions.data.data.gameQuestions.ERROR}
+                  }
+                  return {res: "Se produjo un error"};
+                }
             },
         }
     }
 }
 
-/*
-var service = {
-    ws: {
-        calc: {
-            sumar : function(args) {
-                var n = 1*args.a + 1*args.b;
-                return { sumres : n };
-            },
-
-            multiplicar : function(args) {
-                var n = args.a * args.b;
-                return { mulres : n };
-            }
-        }
-    }
-};*/
 
 var xml = require('fs').readFileSync('service.wsdl', 'utf8'),
 
